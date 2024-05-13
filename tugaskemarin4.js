951e65ff1353bbd12bb0d362d13922435fa066ea
vallen16 <168519272+vallen16@users.noreply.github.com>
2024-05-13 14:14:51 +0700

    Create tugaskemarin4.js

---
+++ tugaskemarin4.js
@@ -0,0 +1,19 @@
+var x = "vallen";
+x+=17;
+console.log(x)
+
+var x = "tino";
+x+=15;
+console.log(x)
+
+var x = "galih";
+x+=10;
+console.log(x)
+
+var x = "bagas";
+x+=11;
+console.log(x)
+
+var x = "radit";
+x+=9;
+console.log(x
