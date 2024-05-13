cd05d21184f91cfc922eaf38ae2e426e5b571483
vallen16 <168519272+vallen16@users.noreply.github.com>
2024-05-13 14:13:59 +0700

    Create tugaskemarin3.js

---
+++ tugaskemarin3.js
@@ -0,0 +1,43 @@
+//let//
+
+let x= 35 + 14
+ x= 10 * 54
+console.log(x)
+
+ x= 261 + 17
+ x+= 49 + 78
+console.log(x)
+
+y= 30 + 10
+ y*= 11 + 3
+console.log(y)
+
+z= 30 - 25
+ z!= 45 + 32
+console.log(z)
+
+x= 156 * 19
+ x/= 20 + 28
+console.log(x)
+
+//var//
+
+var y= 189 + 14
+var y= 34 + 106
+console.log(y)
+
+var y= 10 + 78
+var y= 12 + 10
+console.log(y)
+
+var z= 18 + 23
+var z= 51 + 109
+console.log(z)
+
+var m= 16 + 54
+var m= 15 + 67
+console.log(m)
+
+var w= 19 + 346
+var w= 18 + 167
+console.log(w
