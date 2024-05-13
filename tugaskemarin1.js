df4f6441df4c8aa6954dc3a8beb7fbc6d2e11f0d
vallen16 <168519272+vallen16@users.noreply.github.com>
2024-05-13 14:11:34 +0700

    Create tugaskemarin1.js

---
+++ tugaskemarin1.js
@@ -0,0 +1,9 @@
+var x={nama:"Lana",umur:23};
+console.log(x.umur)
+
+var x={nama:"vallen",umur:17,absen:34,kelas:"rpl3"};
+console.log(x)
+console.log(x.nama)
+console.log(x.umur)
+console.log(x.absen)
+console.log(x.kelas)
